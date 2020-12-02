6688460f9f5ecd534ab587439e47a4323c00b1ba
weekendzhu <249261450@qq.com>
2020-12-03 03:21:55 +0800

    Initial commit

---
+++ src/test/java/com/weekendzhu/demo/DemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.weekendzhu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class DemoApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
